f9a9762ed1dc71aac798516b0b231db2bcedce59
Ian Kenney <ian.kenney@asu.edu>
2016-08-20 18:14:37 -0700

    added logging and setup

--- manifest/Manifest.py
+++ manifest/Manifest.py
@@ -2,6 +2,11 @@ import sys
 import yaml
 import os
 import shutil
+import logging 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("manifest")
+logger.info("Starting manifest logging")
 
 class Manifest(object):
     """Tool for managing a multistate workflow. Requires a properly formatted
@@ -13,17 +18,20 @@ class Manifest(object):
     def __init__(self, filename=None):
         if filename:
             try:
+                logger.info('Loading manifest file \'{}\''.format(filename))
                 with open(filename, 'r') as f:
                     raw = yaml.load(f)
                     self._path = raw.pop('path')
-            except IOError:
-                    print('Cannot open', filename)
-                    raise FileNotFoundError
-            except KeyError:
-                    print('No path provided. Check manifest file.')
-                    raise KeyError
-
+                logger.info('Loaded manifest file')
+            except IOError as e:
+                logger.error('Could not open file \'{}\''.format(filename))
+                raise
+            except KeyError as e:
+                logger.error('No path provided. Check manifest file')
+                raise
+            logger.info('Building states')
             self._build_states(raw)
+            logger.info('States built')
     
     @property
     def path(self):
@@ -40,7 +48,9 @@ class Manifest(object):
         state -- a state from the manifest file.
         dest -- destination path.
         """
+        logger.info('Assembling state \'{}\''.format(state))
         self.states[state].assemble(self._path, dest)
+        logger.info('Assembled state \'{}\''.format(state))
 
     def _build_states(self, data):
         """Iterates through all states in the manifest file and populates the 
@@ -57,7 +67,7 @@ class Manifest(object):
         for state in self.states:
             if self.states[state].broken:
                 self.broken = True
-                print('State {} is broken.'.format(state))
+                logger.warning('State {} is broken.'.format(state))
         if self.broken:
             raise Exception
                 
@@ -68,16 +78,17 @@ class State(object):
         self.name = name
         try:
             self.file_list = values.pop('files')
-            print('\n\033[4m'+"Checking state integrity ({}):".format(self.name)+'\033[0m')
+            logger.info('\n\033[4m'+"Checking state integrity ({}):".format(self.name)+'\033[0m')
             broken = False
 
             for f in self.file_list:
                 exists = os.path.exists(os.path.join(path, name, f[0]))
                 if not exists:
                     broken = True
-                print("   {1:17} <-- {0}".format(f[0], self._format_integrity(exists)))
+                logger.info("   {1:17} <-- {0}".format(f[0], self._format_integrity(exists)))
         except KeyError:
-            print(name, 'missing file list.')
+            logger.error(name, 'missing file list.')
+            raise
 
         self.broken = broken
 
@@ -97,7 +108,7 @@ class State(object):
             if not os.path.exists(os.path.dirname(dest_f)):
                 os.makedirs(os.path.dirname(dest_f))
             shutil.copyfile(src_path_f, dest_f)
-        print("{0} build complete...".format(self.name))
+        logger.info("{0} build complete...".format(self.name))
 
     def _format_integrity(self, exists):
         
--- manifest/__init__.py
+++ manifest/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['manifest']
+from .Manifest import Manifest, State
